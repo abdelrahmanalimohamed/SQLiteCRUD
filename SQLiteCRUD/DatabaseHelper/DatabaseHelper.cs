d059df88fbbb52e36a1cd003a54268966a4cb73e
abdelrahmanalimohamed <abdelrahmanali.mohamed@outlook.com>
2023-11-04 15:57:26 +0200

    Add project files.

---
+++ SQLiteCRUD/DatabaseHelper/DatabaseHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Data.SQLite;
+using System.Data;
+
+namespace SQLiteCRUD.DatabaseHelper
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public void ExecuteNonQuery(string query)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    var dataTable = new DataTable();
+                    using (var adapter = new SQLiteDataAdapter(command))
+                    {
+                        adapter.Fill(dataTable);
+                    }
+                    return dataTable;
+                }
+            }
+        }
+    }
+}
